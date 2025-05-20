24d46dd830e1e383cba381fff5a921f3d566a399
Rainer Santos <rainertoviel@gmail.com>
2025-05-20 23:18:12 +0400

    made the landing page part of the gsap

--- js/gsap-index.js
+++ js/gsap-index.js
@@ -6,30 +6,65 @@ document.addEventListener('DOMContentLoaded', function() {
   // Landing page Intro
   let tl = gsap.timeline();
   
-  // Properly sequenced animations
-  tl.from(".text", {
+  // up the logo and text
+  tl.from(".pre-loader-container", {
     duration: 0.8,
     y: 40,
     opacity: 0,
     ease: "power2.inOut",
     delay: 1
   })
+  tl.from(".pre-logo-img", {
+    duration: 0.8,
+    x: "100px",
+    scale: 1.5,
+    ease: "power2.inOut",
+    delay: 0.5
+  })
+  tl.from(".pre-logo-text", {
+    duration: 0.8,
+    x: 10,
+    opacity: 0,
+    ease: "power2.inOut",
+  })
   .from(".loader", {
     duration: 2,
     width: 0,
     opacity: 0,
-    ease: "power4.inOut"
-  }, "-=0.5") // Starts 0.5s before previous animation ends
+    ease: "power4.inOut",
+    delay: -1,
+  }, "-=0.5") 
   .to(".pre-loader", {
     duration: 2,
     top: "-110%",
-    ease: "power4.inOut"
-  })
+    ease: "power4.inOut",
+    delay: -0.5,
+  })//loader end
+  // landing section
+  tl.from(".landing-title", {
+    duration: 2,
+    x: -45,
+    opacity: 0,
+    ease: "power4.inOut",
+  }, "-=1")
+  tl.from(".landing-text p, .landing-text button", {
+    duration: 2,
+    x: 45,
+    opacity: 0,
+    stagger: 0.25,
+    ease: "power4.inOut",
+  }, "-=1.75")
   tl.from(".landing-section", {
     duration: 2,
     y: -45,
     ease: "power4.inOut",
-}, "-=1")
+  }, "-=1")
+    tl.from("nav", {
+    duration: 2,
+    y: -100,
+    ease: "power4.inOut",
+  }, "-=1.5")
+  
   
 
 
